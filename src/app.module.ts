c689ebe84a69fef43033d5f7b158bb7765f9c61a
TProgramer <tposter@naver.com>
2024-06-12 15:38:25 +0900

    :building_construction: Project domain setup
    도메인:
    	auth: 사용자 정보 관리
    	token: JWT 정보 관리
    	expiry: 만료된 JWT 관리

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { CryptoModule } from './crypto/crypto.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, AuthModule, CryptoModule],
   controllers: [AppController],
   providers: [AppService],
 })
