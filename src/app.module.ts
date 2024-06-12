5ba44d1add65fc9a3ef08a01b734e11da01bbe47
TProgramer <tposter@naver.com>
2024-06-12 17:28:11 +0900

    :wrench: DB 환경변수 설정 및 검증 함수 구현
    + 환경변수 프로필 local 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,23 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { CryptoModule } from './crypto/crypto.module';
+import { ConfigModule } from '@nestjs/config';
+
+import { validate } from './common/env.validator';
+
+import * as path from 'path';
 
 @Module({
-  imports: [UserModule, AuthModule, CryptoModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate,
+      envFilePath: path.resolve(`${__dirname}/../.env.local`),
+    }),
+    UserModule,
+    AuthModule,
+    CryptoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
