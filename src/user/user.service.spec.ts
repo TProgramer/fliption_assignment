f0a0df4b77b657de086cbedc941d4eec91c943eb
TProgrammer <77713508+TProgramer@users.noreply.github.com>
2024-06-13 18:05:51 +0900

    FEAT-회원가입-기능-구현#5 (#8)
    * :test_tube: 회원가입 서비스의 단위테스트 작성
    
    * :necktie: 회원가입 서비스 구현

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,18 +1,36 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 
 describe('UserService', () => {
   let service: UserService;
+  let repository: Repository<User>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [UserService, Repository<User>],
     }).compile();
 
     service = module.get<UserService>(UserService);
+    repository = module.get<Repository<User>>(Repository<User>);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('DB에 존재하지않는 유저라면, 회원가입을 처리한다.', async () => {
+    const userRepositoryFindOneSpy = jest
+      .spyOn(repository, 'findOne')
+      .mockResolvedValue(undefined);
+
+    const savedUser: User = { email: 'test@naver.com', name: 'test' };
+
+    const userRepositorySaveSpy = jest
+      .spyOn(repository, 'save')
+      .mockResolvedValue(savedUser);
+
+    await service.signUp(savedUser);
   });
 });
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Inject, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { DataSource, Repository } from 'typeorm';
+import { Token } from './entities/token.entity';
+import { Expiry } from './entities/expiry.entity';
+import { CryptoManager } from 'src/crypto/provider/crypto-manager';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
+  constructor(
+    @Inject('DATA_SOURCE') private readonly dataSource: DataSource,
+    private readonly cryptoManager: CryptoManager,
+    @Inject(User)
+    private readonly userRepository: Repository<User>,
+    @Inject(Token)
+    private readonly tokenRepository: Repository<Token>,
+    @Inject(Expiry)
+    private readonly expiryRepository: Repository<Expiry>,
+  ) {}
 
-  findAll() {
-    return `This action returns all user`;
-  }
+  async signUp(createUserDto: CreateUserDto) {
+    const didEncryptEmail = await this.cryptoManager.encrypt(
+      createUserDto.email,
+    );
+    const didEncryptName = await this.cryptoManager.encrypt(createUserDto.name);
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
+    // 암호화한 정보가 DB에 이미 존재하는지 확인
+    const user = await this.userRepository.findOne({
+      where: {
+        email: didEncryptEmail,
+        name: didEncryptName,
+      },
+    });
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
+    if (user) {
+      throw new HttpException('이미 가입된 유저입니다.', 400);
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+    // 존재하지 않은 정보라면, DB에 저장하며 가입정보를 반환
+    const didCreateUser = await this.userRepository.save({
+      email: didEncryptEmail,
+      name: didEncryptName,
+    });
+    didCreateUser.email = createUserDto.email;
+    didCreateUser.name = createUserDto.name;
+
+    return didCreateUser;
   }
 }
